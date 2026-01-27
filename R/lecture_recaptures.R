fa6f409e8a3c278e92742e11039cc04228322300
Desgagnés <Mathieu.Desgagnes@dfo-mpo.gc.ca>
2026-01-27 15:35:42 -0500

    lecture et prétraitement des retours de tags, mise en commun avec la bd des longueurs

--- R/lecture_recaptures.R
+++ R/lecture_recaptures.R
@@ -1,8 +1,12 @@
 
 # origine=file.path('S:','Flétan','Retour de marquage','BD_retourDeTag','BD_RetourTag')
 # origine=file.path('S:','Flétan','Retour de marquage','BD_retourDeTag','retour des tags_ALL')
-# destination=file.path('S:','Flétan','Retour de marquage','Analyses','input','recap_vTrav.csv')
+# destination=file.path('S:','Flétan','Retour de marquage','Analyses','input','recap_vTrav')
 
+# le travail de recherche des tags originaux est intégré dans la base de données des retours de tag et
+# il ne devrait pas être nécessaire de combiner la base de données du relevé palangre.
+#
+# format de la sortie:
 
 lecture_recaptures <- function(origine, destination){
     #
@@ -43,6 +47,9 @@ lecture_recaptures <- function(origine, destination){
     ##
     recap.init[is.na(recap.init$tailleCm), 'tailleCm'] <- recap.init[is.na(recap.init$tailleCm), 'taillePo'] * 2.54
     ##
+    recap.init$ffaw <- FALSE
+    recap.init[which(recap.init['prénom']=='FFAW'),'ffaw'] <- TRUE
+    ##
     if(FALSE){
       table(recap.init['prénom'])
       test <- recap.init[recap.init['prénom']=='FFAW',]
@@ -50,7 +57,7 @@ lecture_recaptures <- function(origine, destination){
     }
     ## recap <- subset(recap.init, substr(recap.init$clePoisson,1,1)%in%c('s','d'))
     ## plot(recap$tEnMer.an); hist(recap$tEnMer, breaks=seq(0,3000,by=30)); abline(v=seq(-250,10000,by=365))
-    write.csv2(recap.init, file=destination, row.names=FALSE)
+    write.csv2(recap.init, file=paste0(destination,'.csv'), row.names=FALSE)
     ##
 
 
@@ -59,14 +66,15 @@ lecture_recaptures <- function(origine, destination){
 
 
     ## ###
-    ## Associer le données de marquage
+    ## Associer le données de marquage, issus de la base de données du relevé à la palangre 4RST
     ## ###
     anneeCourante <- max(recap.init$an_recap, na.rm=TRUE)
     load(file.path('S:','Flétan','Relevé 4RST', 'Analyses','input',anneeCourante,'longueursPlusMeta.RData'), verbose=1)
-
+    nrow(longueurs.temp)
+    ##
     tag.temp <- subset(longueurs.temp, !is.na(tag_1) | !is.na(tag_2)); nrow(tag.temp)
     tag.temp$nbTagPose <- as.numeric(!is.na(tag.temp$tag_1)) +  as.numeric(!is.na(tag.temp$tag_2))
-    tag <- tag.temp[,c('cleStation','clePoisson','longueur_cm','blessure','condition','nbTagPose')]
+    tag <- tag.temp[,c('cleStation','clePoisson','annee','dateDescente','noStation','longueur_cm','blessure','condition','nbTagPose','observateur')]
     ##
     ## recap sans longueur associé
     if(FALSE){
@@ -81,45 +89,53 @@ lecture_recaptures <- function(origine, destination){
       table(test2$anMarq)
       sum(table(test2$anMarq)[-c(1:3)])
     }
-
-
-
-    ####
     ##
-    ## Continuer ici
+    if(TRUE){ #identifie les stations à considérer comme "représentatives" de la population, aller chercher dans le fichier des stations
+      tag$pourCalculF <- FALSE
+      length(which(tag$annee>=2017 & tag$noStation<=120 & tag$longueur_cm>=77))/length(which(tag$annee>=2017 & tag$noStation<=120))
+      length(which(tag$annee>=2017 & tag$noStation>120 & tag$longueur_cm>=77))/
+        length(which(tag$annee>=2017 & tag$noStation>120))
+      tag[which(tag$annee>=2017 & as.numeric(tag$noStation)<=120 & tag$longueur_cm>=77),'pourCalculF'] <- TRUE
+      tag$pourCalculF.prime <- FALSE
+      tag[which(tag$annee>=2017 & tag$longueur_cm>=77),'pourCalculF.prime'] <- TRUE
+    }
     ##
-    ####
-
-
-
-    ##
-    tag$annee <- longueurs.temp[match(tag$cleStation, longueurs.temp$cleStation),'annee']
-    tag$date <- longueurs.temp[match(tag$cleStation, longueurs.temp$cleStation),'date']
-    ## tag$strate <- stations[match(tag$cleStation, stations$cleStation),'strate']
-    tag$noStation <- as.numeric(longueurs.temp[match(tag$cleStation, longueurs.temp$cleStation),'noStation'])
-    ## longueurs.temp[match(tag$cleStation, longueurs.temp$cleStation),'noStation'][is.na(as.numeric(longueurs.temp[match(tag$cleStation, longueurs.temp$cleStation),'noStation']))]
-    ##
-    tag$pourCalculF <- FALSE
-    length(which(tag$annee>=2017 & tag$noStation<=120 & tag$longueur_cm>=77))/length(which(tag$annee>=2017 & tag$noStation<=120))
-    tag[which(tag$annee>=2017 & tag$noStation<=120 & tag$longueur_cm>=77),'pourCalculF'] <- TRUE
-    tag$pourCalculF.prime <- FALSE
-    tag[which(tag$annee>=2017 & tag$longueur_cm>=77),'pourCalculF.prime'] <- TRUE
-
     ## ajouter recaptures
     ## table(recap.init$clePoisson, useNA='ifany')
-    tag$anneeRecap <- recap.init[match(tag$clePoisson, recap.init$clePoisson),'anRecap']
-    tag$longueur_cmRecap <- recap.init[match(tag$clePoisson, recap.init$clePoisson),'tailleCm']
-    tag$nbTagRecap <- recap.init[match(tag$clePoisson, recap.init$clePoisson),'nbTagRecap']
+    lesquels <- match(tag$clePoisson, recap.init$clePoisson)
+    tag[which(!is.na(lesquels)),'anneeRecap'] <- recap.init[na.omit(lesquels),'an_recap']
+    ##
+    tag[which(!is.na(lesquels)),'longueur_cmRecap'] <- recap.init[na.omit(lesquels),'taille_cm']
+    tag[which(!is.na(lesquels)),'nbTagRecap'] <- recap.init[na.omit(lesquels),'nbTagRecap']
     ## tag$nbTagPerdu <- recap.init[match(tag$clePoisson, recap.init$clePoisson),'nbPerdu']
     tag$nbTagPerdu <- tag$nbTagPose - tag$nbTagRecap
-    tag$tEnMer <- recap.init[match(tag$clePoisson, recap.init$clePoisson),'tEnMer']
+    tag[which(!is.na(lesquels)),'tEnMer'] <- recap.init[na.omit(lesquels),'tEnMer']
     tag$nbAnEnMer <- tag$anneeRecap-tag$annee
-    tag$tEnMer.an <- recap.init[match(tag$clePoisson, recap.init$clePoisson),'tEnMer.an']
+    tag[which(!is.na(lesquels)),'tEnMer.an'] <- recap.init[na.omit(lesquels),'tEnMer.an']
 
-    ## ## ajouter observateur-marqueurs a tag
-    ## tag$marqueur <- stations[match(tag$cleStation, stations$cleStation),'observateur']
+    if(FALSE){ #validation visuelle des la séparation des temps en mer en années
+      par(mfrow=c(1,2))
+      hist(tag$tEnMer, breaks=seq(0,4100, by=365/12)); abline(v=-240+365*0:12, col=2); text(x=-60+365*0:12, y=par('usr')[4]*0.95, labels=0:12)
+      hist(subset(tag, nbTagPerdu==1)$tEnMer, breaks=seq(0,4100, by=365/12), col=2, add=TRUE)
+      hist(subset(tag, tag$pourCalculF)$tEnMer, breaks=seq(0,4100, by=365/12)); abline(v=-240+365*0:10, col=2); text(x=-60+365*0:10, y=par('usr')[4]*0.95, labels=0:10)
+      hist(subset(tag, tag$pourCalculF & nbTagPerdu==1)$tEnMer, breaks=seq(0,4100, by=365/12), col=2, add=TRUE)
+      ## hist(subset(tag, tag$pourCalculF.prime)$tEnMer, breaks=seq(0,4000, by=365/12)); abline(v=-240+365*0:10, col=2); text(x=-60+365*0:10, y=par('usr')[4]*0.95, labels=0:10)
+      ## hist(subset(tag, tag$pourCalculF.prime & nbTagPerdu==1)$tEnMer, breaks=seq(0,4000, by=365/12), col=2, add=TRUE)
+      ## plot(tag$annee, tag$tEnMer)
+    }
+    ##
     ## ## évaluer les taux de retour
-    ## table(tag$marqueur, tag$annee)
+   ## table(tag$observateur, tag$annee)
+
+    tagRelPal <- tag[order(tag$date),]
+    write.csv2(tagRelPal, paste0(destination,'_bgTag.csv'), row.names=FALSE)
+    save(tagRelPal, file=paste0(destination,'_bdTag.RData'))
+
+    ## pour calcul de perte de tag
+    perteTag <- na.omit(tagRelPal[tagRelPal$nbTagPose>1, c('nbTagPose','nbTagRecap','nbTagPerdu','nbAnEnMer')]); nrow(perteTag)
+    write.csv2(perteTag, file=paste(destination, '_perteTag.csv'), row.names=FALSE)
+    tagF <- subset(tag[order(tag$date),], pourCalculF==TRUE)
+    write.csv2(tagF, file=paste0(destination, '_bdTagPourF.csv'), row.names=FALSE)
 
 
     return(recap.init)
